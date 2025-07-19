642d99b7602c4ccf4ac0e6c352dea00eb13002db
pradumn26 <pradumn.26@gmail.com>
2025-07-19 12:54:27 +0530

    feat: make headless true

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,8 @@ const crawler = new PlaywrightCrawler({
     requestHandler: router,
     requestHandlerTimeoutSecs: 1800,
     // For local development, it's good to keep the browser visible
-    headless: false,
-    // headless: true,
+    // headless: false,
+    headless: true,
     launchContext: {
         userDataDir: './user-data',
         launchOptions: {
